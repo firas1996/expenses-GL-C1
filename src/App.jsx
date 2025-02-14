722af80e588860ebf0ffc6f0d0365507b4d68ff8
firas hamila <firas.hm.1996@gmail.com>
2025-02-14 15:54:07 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,37 @@ import MyFrag from "./components/MyFrag";
 import ExpenseInfo from "./components/ExpenseInfo";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      title: "Voyage",
+      price: 350,
+      date: new Date("2025-02-14"),
+    },
+    {
+      id: 2,
+      title: "Shopping",
+      price: 150,
+      date: new Date("2024-10-18"),
+    },
+    {
+      id: 3,
+      title: "Car",
+      price: 100,
+      date: new Date("2025-05-14"),
+    },
+    {
+      id: 4,
+      title: "Education",
+      price: 300,
+      date: new Date("2023-10-14"),
+    },
+  ];
   return (
     <>
+      <ExpenseInfo />
+      <ExpenseInfo />
+      <ExpenseInfo />
       <ExpenseInfo />
     </>
   );
