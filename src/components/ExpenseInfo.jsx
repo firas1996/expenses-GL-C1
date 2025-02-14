a2f946d9dbe92411a7a2b60e2682fce041aff787
firas hamila <firas.hm.1996@gmail.com>
2025-02-14 16:39:00 +0100

    a

--- src/components/ExpenseInfo.jsx
+++ src/components/ExpenseInfo.jsx
@@ -3,7 +3,7 @@ import "./ExpenseInfo.css";
 const ExpenseInfo = ({ title, price, date }) => {
   return (
     <div className="expense-item">
-      <div className="expense-date">date</div>
+      <div className="expense-date">{date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">$ {price}</div>
