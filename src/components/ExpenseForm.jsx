4d57817f40e799b2d1659728363cd76d0c0da9e9
firas hamila <firas.hm.1996@gmail.com>
2025-03-07 10:01:51 +0100

    a

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   const thisYear = new Date().getFullYear();
   const minDate = `${thisYear - 2}-01-01`;
   const maxDate = `${thisYear + 2}-12-31`;
+  const [formData, setFormData] = useState({
+    title: "",
+    price: "",
+    date: "",
+  });
+  const handelChange = ({ target }) => {
+    const { name, value } = target;
+    setFormData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
   return (
     <div className="expense-form">
       <form>
         <div className="expense-form__controls">
           <div className="expense-form__control">
             <label>Title</label>
-            <input required placeholder="Title" />
+            <input
+              required
+              placeholder="Title"
+              name="title"
+              onChange={handelChange}
+            />
           </div>
           <div className="expense-form__control">
             <label>Price</label>
@@ -20,11 +37,20 @@ const ExpenseForm = () => {
               type="number"
               min="0"
               step="0.01"
+              name="price"
+              onChange={handelChange}
             />
           </div>
           <div className="expense-form__control">
             <label>Date</label>
-            <input required type="date" min={minDate} max={maxDate} />
+            <input
+              required
+              type="date"
+              min={minDate}
+              max={maxDate}
+              name="date"
+              onChange={handelChange}
+            />
           </div>
         </div>
         <div className="expense-form__actions">
